84bb0b54c7b64274fc835c9d53e4e8f53e04e6b6
Max Bagrov <wisdom683@rambler.ru>
2015-01-28 20:11:17 +0300

    Logic for folder browsing is pulled to single module

--- src/main/java/alvion/controller/HelloController.java
+++ src/main/java/alvion/controller/HelloController.java
@@ -1,24 +1,22 @@
 package alvion.controller;
 
-import alvion.model.FileInfo;
-import alvion.service.FileListService;
-import alvion.service.FolderService;
+import alvion.service.HelloService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
-import java.io.UnsupportedEncodingException;
-import java.net.URLDecoder;
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
-@RequestMapping("/")
+@RequestMapping("/hello")
 public class HelloController {
 
-   /* @Autowired
+    @Autowired
     private HelloService helloService;
 
     @RequestMapping(method = RequestMethod.GET)
@@ -30,7 +28,7 @@ public class HelloController {
 	}
 
     @RequestMapping(value = "list/**", method = RequestMethod.GET)
-    public ModelAndView list(HttpServletRequest request) {
+    public ModelAndView list(@PathVariable String name, HttpServletRequest request) {
 
         System.out.println(request.getRequestURI());
         System.out.println(request.getRequestURL().toString());
@@ -40,49 +38,9 @@ public class HelloController {
         list.add("String 1");
         list.add("String 2");
         list.add("String 3");
-     //   list.add(name);
+        list.add(name);
         model.addObject("list", list);
         return model;
 
-    }*/
-
-    @Autowired
-    private FileListService fileListService;
-
-    @Autowired
-    private FolderService folderService;
-
-    @RequestMapping(method = RequestMethod.GET)
-    public ModelAndView init() throws UnsupportedEncodingException {
-        return showIndexPage(null);
-    }
-
-    @RequestMapping(value = "list/**", method = RequestMethod.GET)
-    public ModelAndView showIndexPage(HttpServletRequest request) throws UnsupportedEncodingException {
-        ModelAndView model = new ModelAndView("index");
-        String nextFolderPath = "";
-        String curFolderPath = "";
-
-        if (request == null) {
-            nextFolderPath = "C:\\";
-            curFolderPath = "";
-        } else {
-          //  System.out.println(request.getRequestURI());
-          //  System.out.println("Current folder " + folderService.getCurrentFolderPath(request.getRequestURI(),"list/"));
-          //  System.out.println("Next folder " + folderService.getNextFolderPath(request.getRequestURI(),"list/"));
-            curFolderPath = folderService.getCurrentFolderPath(request.getRequestURI(), "list/");
-            nextFolderPath = folderService.getNextFolderPath(URLDecoder.decode(request.getRequestURI(), "UTF-8"),"list/");
-        }
-        try {
-            List<FileInfo> fileList = fileListService.getFileList(nextFolderPath);
-            model.addObject("fileList", fileList);
-            model.addObject("prevFolder", curFolderPath);
-        } catch(Exception e) {
-          //  System.out.println("Here it is");
-            model = new ModelAndView("index500");
-            model.addObject("prevFolder", curFolderPath);
-        }
-
-        return model;
     }
 }
