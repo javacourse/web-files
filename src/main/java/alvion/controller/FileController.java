122df0bf3d11176f8f04f4175f22e17d473d59b8
Eugene Lazarchuk <shadyjk@yandex.ru>
2015-02-03 19:10:31 +0300

    update

--- src/main/java/alvion/controller/FileController.java
+++ src/main/java/alvion/controller/FileController.java
@@ -2,12 +2,16 @@ package alvion.controller;
 
 import alvion.service.FolderService;
 import java.io.FileNotFoundException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 @RequestMapping("/dirviewer/")
@@ -16,12 +20,17 @@ public class FileController {
     @Autowired
     private FolderService folderService;
     
-    @RequestMapping(method = RequestMethod.GET)
+/*    @RequestMapping(method = RequestMethod.GET)
     public ModelAndView list() {
-        return list("");
+        return new ModelAndView("redirect:/path/");
 	}
-    @RequestMapping(value = "{dir}", method = RequestMethod.GET)
-    public ModelAndView list(@PathVariable String dir) {
+    */
+    
+    @RequestMapping(value = "**", method = RequestMethod.GET)
+    public ModelAndView list(HttpServletRequest request) throws UnsupportedEncodingException {
+        String dir = request.getRequestURI().toString().replaceAll("/web-files/dirviewer", "");
+        dir = URLDecoder.decode(dir, "UTF-8");
+        
         ModelAndView model = new ModelAndView("dirviewer");
         try {
             folderService.setFolder("/".concat(dir));
